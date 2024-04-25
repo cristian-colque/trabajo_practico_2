bd5aa1980299be74c907edc9c94e5397e16e79f1
cristian <melopartituras@gmail.com>
2024-04-25 16:56:56 -0300

    avance ejercicio 01

--- tp2_ColqueCristian/src/ar/edu/unju/fi/ejercicio01/model/Producto.java
+++ tp2_ColqueCristian/src/ar/edu/unju/fi/ejercicio01/model/Producto.java
@@ -1,5 +1,26 @@
 package ar.edu.unju.fi.ejercicio01.model;
 
 public class Producto {
-
+	
+	private String codigo;
+	private String descripcion;
+	private double precioUnitario;
+	private OrigenFabricacion origenFabricacion;
+	private Categoria categoria;
+	
+	
+	public Producto(String codigo, String descripcion, double precioUnitario, OrigenFabricacion origenFabricacion,
+			Categoria categoria) {
+		this.codigo = codigo;
+		this.descripcion = descripcion;
+		this.precioUnitario = precioUnitario;
+		this.origenFabricacion = origenFabricacion;
+		this.categoria = categoria;
+	}
+	public enum OrigenFabricacion{
+		ARGENTINA, CHINA, BRASIL, URUGUAY
+	}
+	public enum Categoria{
+		TELEFONIA, INFORMATICA, ELECTROHOGAR, HERRAMIENTAS
+	}
 }
