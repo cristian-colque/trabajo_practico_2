8235505d8e46823135de4917d2ed1f830fd362f1
cristian <melopartituras@gmail.com>
2024-05-05 23:12:10 -0300

    ejericio 01 completo

--- tp2_ColqueCristian/src/ar/edu/unju/fi/ejercicio01/model/Producto.java
+++ tp2_ColqueCristian/src/ar/edu/unju/fi/ejercicio01/model/Producto.java
@@ -23,4 +23,57 @@ public class Producto {
 	public enum Categoria{
 		TELEFONIA, INFORMATICA, ELECTROHOGAR, HERRAMIENTAS
 	}
+	
+	 public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public double getPrecioUnitario() {
+		return precioUnitario;
+	}
+
+	public void setPrecioUnitario(double precioUnitario) {
+		this.precioUnitario = precioUnitario;
+	}
+
+	public OrigenFabricacion getOrigenFabricacion() {
+		return origenFabricacion;
+	}
+
+	public void setOrigenFabricacion(OrigenFabricacion origenFabricacion) {
+		this.origenFabricacion = origenFabricacion;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	@Override
+	    public String toString() {
+	        return "Producto{" +
+	                "codigo='" + codigo + '\'' +
+	                ", descripcion='" + descripcion + '\'' +
+	                ", precioUnitario=" + precioUnitario +
+	                ", origenFabricacion=" + origenFabricacion +
+	                ", categoria=" + categoria +
+	                '}';
+	    }	 
 }
+
+
